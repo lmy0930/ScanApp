1d4517b723b2388131aa77fd8b20932da563dad9
lmy0930 <lmy0930@126.com>
2016-10-09 17:26:21 +0800

    2016/10/9_17.26

--- src/com/fx/scanapp/ResponsibiltyFactory.java
+++ src/com/fx/scanapp/ResponsibiltyFactory.java
@@ -2,6 +2,7 @@ package com.fx.scanapp;
 
 import com.fx.scanapp.CHG_CLS.CLSChain;
 import com.fx.scanapp.CHG_KP.KPChain;
+import com.fx.scanapp.CHG_WO.CHGWOChain;
 import com.fx.scanapp.Init.InitChain;
 import com.fx.scanapp.SCARCITY.SCARCITYChain;
 
@@ -41,12 +42,12 @@ public class ResponsibiltyFactory {
 		     CLSChain clschain=new CLSChain();
 		     TaskChain=clschain.getChain();
 		 }
-		// else
-		// if(action.equals("ACTION-CHG-WO")&&Machine.getInstance().cmand_status==0)//产生换线子连
-		// {
-		// InitChain initchain=new InitChain();
-		// TaskChain=initchain.getChain();
-		// }
+		 else
+		 if(action.equals("ACTION-CHG-WO")&&Machine.getInstance().cmand_status==0)//产生换线子连
+		 {
+		     CHGWOChain CHGWOchain=new CHGWOChain();
+		     TaskChain=CHGWOchain.getChain();
+		 }
 		else if ((action.equals("ACTION-CHG-KP")&& Machine.getInstance().cmand_status == 0))// 产生换料子连
 		{
 			KPChain kpchain = new KPChain();
